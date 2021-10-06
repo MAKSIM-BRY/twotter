9a9008556e050a149ad5faa4c3ed3dafb2b9b4b8
MAKSIM-BRY <maxim.barry@gmail.com>
2021-10-06 23:39:41 +0200

    Mise en place d'une nouvelle route /assets pour les images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import twotterLogo from '../../assets/twotterLogo.png';
 
 function Header() {
   return (
     <header className="Header">
-      <img className="logo" src="./twotterLogo.png" alt="logo" />
+      <img className="logo" src={twotterLogo} alt="logo" />
       <span className="title">Twotter</span>
     </header>
   );
